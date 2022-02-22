2fe21a121c10e7d994f48101820f86028bfa226b
David GABISON <david.gabison@outlook.com>
2022-02-22 05:28:42 +0100

    feat: add Model.getAllTemplates method

--- src/lib/Model.ts
+++ src/lib/Model.ts
@@ -1,9 +1,13 @@
 import Template from './Template';
-import { todo } from './util';
+import type { ModelModule } from './types';
+import { getConfig, getLogger } from './util';
+
+const log = getLogger('Model');
 
 export default class Model {
   private readonly modulepath: string;
   private readonly name: string;
+  private allTemplates?: Template[];
 
   public constructor (modulepath: string, name: string) {
     this.modulepath = modulepath;
@@ -11,10 +15,18 @@ export default class Model {
   }
 
   public async getAllTemplates (): Promise<Template[]> {
-    return await Promise.resolve(todo(this, Template) as Template[]);
+    if (!this.allTemplates) this.allTemplates = await this._getAllTemplates();
+    return this.allTemplates;
   }
 
   public getName (): string {
     return this.name;
+  }
+
+  private async _getAllTemplates (): Promise<Template[]> {
+    log('getAllTemplates');
+    const model = await import(this.modulepath) as ModelModule;
+    const templates = await (typeof model === 'function' ? model(getConfig()) : model);
+    return (Array.isArray(templates) ? templates : [templates]).map(raw => new Template(raw, this));
   }
 }
