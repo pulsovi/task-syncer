97f5c73314ee33ba6720e25521939e8ce6fb756a
David GABISON <david.gabison@outlook.com>
2022-03-11 17:11:13 +0100

    feat: complete fix-with-test to verify that test fails before commit

--- src/bin/fix-with-test.ts
+++ src/bin/fix-with-test.ts
@@ -8,8 +8,6 @@ import chalk from 'chalk';
 import { program } from 'commander';
 import fs from 'fs-extra';
 
-import { todo } from '../lib/util';
-
 program
   .name('commit-msg');
 
@@ -20,7 +18,10 @@ program
     const gitRoot = await cmdOut('git rev-parse --show-toplevel');
     const message = await fs.readFile(path.join(gitRoot, commitMsgFile), 'utf8');
 
-    if (!message.startsWith('fix:')) return;
+    if (!message.startsWith('fix:')) {
+      console.log('The commit is not a fix, exit OK');
+      return;
+    }
 
     const status = await cmdOut('git status --porcelain');
     const filesCommited = status.split('\n')
@@ -28,20 +29,45 @@ program
       .map(line => line.slice(3));
     const testAddedFiles = filesCommited.filter(filename => filename.endsWith('.test.ts'));
 
-    if (!testAddedFiles.length)
-      throw new Error('Le commit est de type "fix: " il DOIT ajouter un test.');
+    if (!testAddedFiles.length) {
+      console.log('No test file added nore modified');
+      throw new Error('fix commits MUST add at least one test that fail before the commit');
+    }
 
     // inutile de vérifier que les tests ajoutés passent après le commit, lint-staged s'en charge
-    todo('Verifier que le/les tests ajoutés échouent avant le commit');
-    // const currentCommit = await cmdOut('git rev-parse --verify --short HEAD');
+    console.log('fix commit', testAddedFiles.length, 'tests files found');
+    await cmdOut('git stash -uq');
+    // const stashHash = await cmdOut('git rev-parse stash@{0}');
+    const stashIndexHash = await cmdOut('git rev-parse "stash@{0}^2"');
+    await cmdOut(`git checkout ${stashIndexHash} -- ${
+      testAddedFiles
+        .map(file => `"${path.relative(process.cwd(), path.join(gitRoot, file))}"`)
+        .join(' ')
+    }`);
+    await cmdOut(`git tag -f dd ${stashIndexHash}`);
+    const jest = await cmdOut('yarn jest --bail').then(
+      value => ({ error: null, value }),
+      (error: unknown) => ({ error, value: null })
+    );
+    if (jest.value) console.log('FAIL'); else console.log('SUCCESS');
+    await cmdOut(`git checkout -- "${path.relative(process.cwd(), gitRoot)}"`);
+    await cmdOut('git stash pop');
+    await cmdOut('git reset');
+    await gitSetIndexToCommit(stashIndexHash);
+    if (jest.value) {
+      console.log('FAIL: fix commits MUST add at least one test that fail before the commit, all this tests pass');
+      process.exit(1);
+    }
   });
 
 program
   .parseAsync(process.argv)
   .catch((error: unknown) => {
     console.log('===============================================');
-    if (error instanceof Error)
+    if (error instanceof Error) {
       console.log(`${chalk.red(error.name)}: ${chalk.yellow(error.message)}`);
+      console.log(error.stack);
+    }
     else
       console.log(error);
     console.log('===============================================');
@@ -49,5 +75,21 @@ program
   });
 
 async function cmdOut (cmd: string): Promise<string> {
-  return (await promisify(exec)(cmd)).stdout.trim();
+  return (await promisify(exec)(cmd)).stdout.slice(0, -1);
+}
+
+async function gitSetIndexToCommit (commit: string): Promise<void> {
+  const index = (await cmdOut(`git show --raw ${commit}`))
+    .split('\n')
+    .filter(line => line.startsWith(':'))
+    .map(line => line.split(' '))
+    .map(([_bmode, amode, _bhash, ahash, file]) => ({
+      file: file.split('\t')[1],
+      mode: amode,
+      thash: ahash,
+    }));
+  await Promise.all(index.map(async ({ mode, thash, file }) => {
+    const hash = await cmdOut(`git rev-parse ${thash}`);
+    await cmdOut(`git update-index --add --cacheinfo ${mode},${hash},${file}`);
+  }));
 }
