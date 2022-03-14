3c039c2fcc197c54de860a484c502a57be8b7cc1
David GABISON <david.gabison@outlook.com>
2022-03-14 10:02:46 +0100

    refactor: use better git commands

--- src/bin/fix-with-test.ts
+++ src/bin/fix-with-test.ts
@@ -34,26 +34,25 @@ program
       throw new Error('fix commits MUST add at least one test that fail before the commit');
     }
 
-    // inutile de vérifier que les tests ajoutés passent après le commit, lint-staged s'en charge
     console.log('fix commit', testAddedFiles.length, 'tests files found');
     await cmdOut('git stash -uq');
-    // const stashHash = await cmdOut('git rev-parse stash@{0}');
+    const stashHash = await cmdOut('git rev-parse stash@{0}');
     const stashIndexHash = await cmdOut('git rev-parse "stash@{0}^2"');
+
     await cmdOut(`git checkout ${stashIndexHash} -- ${
       testAddedFiles
         .map(file => `"${path.relative(process.cwd(), path.join(gitRoot, file))}"`)
         .join(' ')
     }`);
-    // await cmdOut(`git tag -f dd ${stashIndexHash}`);
     const jest = await cmdOut('yarn jest --bail').then(
       value => ({ error: null, value }),
       (error: unknown) => ({ error, value: null })
     );
+    // Les tests doivent ÉCHOUER à ce stade (avant le commit)
     if (jest.value) console.log('FAIL'); else console.log('SUCCESS');
-    await cmdOut(`git checkout -- "${path.relative(process.cwd(), gitRoot)}"`);
-    await cmdOut('git stash pop');
-    await cmdOut('git reset');
-    await gitSetIndexToCommit(stashIndexHash);
+    // inutile de vérifier que les tests passent après le commit, lint-staged s'en charge
+    await cmdOut('git reset --hard');
+    await cmdOut(`git stash pop --quiet --index ${stashHash}`);
     if (jest.value) {
       console.log('FAIL: fix commits MUST add at least one test that fail before the commit, all this tests pass');
       process.exit(1);
@@ -76,20 +75,4 @@ program
 
 async function cmdOut (cmd: string): Promise<string> {
   return (await promisify(exec)(cmd)).stdout.slice(0, -1);
-}
-
-async function gitSetIndexToCommit (commit: string): Promise<void> {
-  const index = (await cmdOut(`git show --raw ${commit}`))
-    .split('\n')
-    .filter(line => line.startsWith(':'))
-    .map(line => line.split(' '))
-    .map(([_bmode, amode, _bhash, ahash, file]) => ({
-      file: file.split('\t')[1],
-      mode: amode,
-      thash: ahash,
-    }));
-  await Promise.all(index.map(async ({ mode, thash, file }) => {
-    const hash = await cmdOut(`git rev-parse ${thash}`);
-    await cmdOut(`git update-index --add --cacheinfo ${mode},${hash},${file}`);
-  }));
 }
