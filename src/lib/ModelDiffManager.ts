dceac1413f5365d12f8fccb7656db447bf4811f1
David GABISON <david.gabison@outlook.com>
2022-03-29 10:19:52 +0200

    feat(anki-pug): implement ModelDiffManager.isManageable method

--- src/lib/ModelDiffManager.ts
+++ src/lib/ModelDiffManager.ts
@@ -1,15 +1,19 @@
 import path from 'path';
 
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+
 import type DiffConfig from './DiffConfig';
 import type Model from './Model';
 import TemplateDiffManager from './TemplateDiffManager';
-import { getLogger, todo } from './util';
+import { getLogger } from './util';
 import type { TaskSyncer } from './util';
 
 const log = getLogger(path.basename(__filename, path.extname(__filename)));
 
 export default class ModelDiffManager {
   private readonly model: Model;
+  private prompted = false;
 
   public constructor (model: Model) {
     this.model = model;
@@ -26,7 +30,55 @@ export default class ModelDiffManager {
     }));
   }
 
-  public async isManageable (diffConfig: DiffConfig, _syncer: TaskSyncer): Promise<boolean> {
-    return await Promise.resolve(todo(this, diffConfig) as boolean);
+  public async isManageable (diffConfig: DiffConfig, ticket: TaskSyncer): Promise<boolean> {
+    if (diffConfig.haveToQuit()) return false;
+    if (diffConfig.isModelManageable(this.model.getName())) {
+      await ticket.ready;
+      this.prompt();
+      return true;
+    }
+    return await this.askManageable(diffConfig, ticket);
+  }
+
+  private async askManageable (diffConfig: DiffConfig, ticket: TaskSyncer): Promise<boolean> {
+    await ticket.ready;
+    const name = this.model.getName();
+    const response = await inquirer.prompt({
+      /* eslint-disable sort-keys */
+      type: 'expand',
+      name: 'manage',
+      message: `Manage ${name} ?`,
+      choices: [
+        { key: 'y', name: `[yes]  Manage ${name}`, value: 'yes' },
+        { key: 'n', name: '[no]   Skip this model', value: 'no' },
+        { key: 'a', name: "[all]  Manage all models (don't ask more)", value: 'all' },
+        { key: 'q', name: '[quit] Skip all unmanaged models and quit the diff', value: 'quit' },
+      ],
+      'default': 2,
+      /* eslint-enable sort-keys */
+    });
+
+    switch (response.manage as string) {
+    case 'quit':
+      diffConfig.haveToQuit(true);
+      // fallthrough: continue to 'no' case
+    case 'no':
+      diffConfig.isModelManageable(name, false);
+      return false;
+    case 'all':
+      diffConfig.manageAllModels();
+      // fallthrough: continue to 'y' case
+    case 'yes':
+      diffConfig.isModelManageable(name, true);
+      return true;
+    default:
+      throw new Error(`Unexpected response : "${String(response.manage)}"`);
+    }
+  }
+
+  private prompt (): void {
+    if (this.prompted) return;
+    console.info(chalk.cyanBright(this.model.getName()));
+    this.prompted = true;
   }
 }
