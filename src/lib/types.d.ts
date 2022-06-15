5024638aa2b9263c0ca51885b705dfe52a1b35bd
David GABISON <david.gabison@outlook.com>
2022-06-15 12:22:12 +0200

    chore: remove unnecessary types

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,9 +1,2 @@
 /* eslint-disable @typescript-eslint/no-type-alias */
-import type { AnkiPugConfig } from './getConfig';
-import type { RawTemplate } from './Template';
-
 type SyncOrPromise<T> = Promise<T> | T;
-type SingleOrArray<T> = T | T[];
-type DirectOrCallback<T> = T | ((config: AnkiPugConfig) => T);
-
-declare type ModelModule = DirectOrCallback<SyncOrPromise<SingleOrArray<RawTemplate>>>;
